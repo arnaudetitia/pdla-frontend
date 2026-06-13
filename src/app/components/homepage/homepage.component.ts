a7183b59ee25f5aa4518f2b587ba2ca5ce579e64
arnaudetitia <arnaud.etitia@gmail.com>
2026-06-13 17:21:42 +0200

    affichage du tableau

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage.component',
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {
-
-}
+export class HomepageComponent {}
