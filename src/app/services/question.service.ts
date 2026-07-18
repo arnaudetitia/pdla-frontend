e66ef46ee3994d48b81f159ab78a41af9281139b
arnaudetitia <arnaud.etitia@gmail.com>
2026-07-18 19:30:49 +0200

    config prod

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.dev';
+import { environment } from '../../environments/environment';
 import { Question, QuestionVo } from '../model/question.model';
 import { ImportError } from '../model/import-errors.model';
 
