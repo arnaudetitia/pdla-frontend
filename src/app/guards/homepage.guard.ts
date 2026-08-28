359e7b19e0ca9e0333e6f9ba8ca8a84fd2b40f65
arnaudetitia <arnaud.etitia@gmail.com>
2026-08-28 21:24:41 +0200

    amélioration du guard

--- src/app/guards/homepage.guard.ts
+++ src/app/guards/homepage.guard.ts
@@ -1,16 +1,26 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { inject } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { catchError, map, of } from 'rxjs';
+
+const ADMIN_PASSWORD = 'ADMIN_PASSWORD';
 
 export const homepageGuard: CanActivateFn = () => {
   if (environment.production) {
     const router = inject(Router);
-    const mdpAdmin = prompt('Mot de passe pour acceder au site :');
-    if (mdpAdmin?.localeCompare('Coucou') === 0) {
-      return true;
-    } else {
-      return router.parseUrl('/403');
+    const adminService = inject(AdminService);
+    let mdpAdmin = localStorage.getItem(ADMIN_PASSWORD);
+    if (!mdpAdmin) {
+      mdpAdmin = prompt('Mot de passe pour acceder au site :');
     }
+    return adminService.checkAdmin(mdpAdmin || '').pipe(
+      map(() => {
+        localStorage.setItem(ADMIN_PASSWORD, mdpAdmin || '');
+        return true;
+      }),
+      catchError(() => of(router.parseUrl('/403'))),
+    );
   }
 
   return true;
